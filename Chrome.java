f2a9ffcb1e67e3ef462ace8baf1ce720e306ae10
Renuga-123 <146524323+Renuga-123@users.noreply.github.com>
2025-10-11 20:51:34 +0530

    Add files via upload
    Chrome.java

---
+++ Chrome.java
@@ -0,0 +1,22 @@
+package Week1;
+
+public class Chrome 
+{
+
+	public static void main(String[] args) 
+	{
+		float versiontype = 91.0f;
+		String developeroftype ="Google";
+		boolean isBeta = false;
+		int releaseYear= 2008;
+		char ShortcutKey='C';
+		System.out.println("versiontype:"+versiontype);
+		System.out.println("developeroftype:"+developeroftype);
+		System.out.println("isBeta:"+ isBeta);
+		System.out.println("releaseYear:"+releaseYear);
+		System.out.println("ShortcutKey:"+ShortcutKey);
+		
+
+	}
+
+}
